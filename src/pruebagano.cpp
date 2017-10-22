db8a974b9395b9e433251386acbdfc72776a32c4
nicocheh <nicocheh@hotmail.com>
2017-10-22 11:47:44 -0300

    probe la funcibe la funcion ganajugador y me convenci de que anda bien

--- src/pruebagano.cpp
+++ src/pruebagano.cpp
@@ -7,40 +7,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-using namespace std;
-
-std::random_device rd;
-std::mt19937 generator(rd());
-
-void send(const std::string& msg) {
-    std::cout << msg << std::endl;
-}
-
-void send(int msg) {
-    std::cout << msg << std::endl;
-}
-
-int read_int() {
-    std::string msg;
-    std::cin >> msg;
-    if (msg == "salir") {
-        send("listo");
-        std::exit(0);
-    }
-    return std::stoi(msg);
-}
-
-std::string read_str() {
-    std::string msg;
-    std::cin >> msg;
-    if (msg == "salir") {
-        send("listo");
-        std::exit(0);
-    }
-    return msg;
-}
-
-
+using namespace std ;
 
 
 bool ganojugador(vector<vector<int>> tablero, int i, int c, int ultimajugada){
@@ -57,6 +24,7 @@ bool ganojugador(vector<vector<int>> tablero, int i, int c, int ultimajugada){
 	 * ACA SI FUESE CUATRO EN LINEA, GANO EL 2 PORQUE TIRO UNA DIAGONAL DE 4. SI LLAMAS GANOJUGADOR(TABLERO,2)
 	 * DEBERIA DAR TRUE
 	 * */
+	 
      if(ultimajugada==-1){return false;}
      //Por si es la primer jugada, nadie gano aun
 	 if (tablero[ultimajugada][tablero[ultimajugada].size()-1]!=i) return false; //tablero[j].size()-1 es la ultima fila con fichas de la columna j
@@ -69,7 +37,7 @@ bool ganojugador(vector<vector<int>> tablero, int i, int c, int ultimajugada){
 		 }else{
 			 int k=tablero[ultimajugada].size()-1; //k es la fila que voy a chequear ahora
 			 while (tablero[ultimajugada].size()-k<=c && esMio){
-				 if (tablero[ultimajugada][k] != i) esMio=false;
+				 if (tablero[ultimajugada][k] != i) esMio=false;		 
 				 //CAMBIO CHEBAR, DECIA TABLERO[TABLERO.SIZE()] DABA SEGFAULT, IBA ESTO, YA LO CAMBIE
 				 --k;
 			 }
@@ -172,7 +140,7 @@ bool ganojugador(vector<vector<int>> tablero, int i, int c, int ultimajugada){
 		 while (esMio && col<tablero.size() && fil>=0 && contador<c){
 			 //calculamos a la derecha de j
 			 if (tablero[col].size()>=tablero[ultimajugada].size()-(col-ultimajugada) && tablero[col][fil]==i){
-				 //CHEBAR, DEBUGEANDO, CREO ES AL REVES LA RESTA DE COL Y ULTIMA JUGADA, ESTABA ASI:
+				  //CHEBAR, DEBUGEANDO, CREO ES AL REVES LA RESTA DE COL Y ULTIMA JUGADA, ESTABA ASI:
 				 
 				 //-(ultimajugada-col)
 				 ++contador;
@@ -196,122 +164,21 @@ bool perdi (const vector<vector<int>>& tablero, int c, int ultimajugada){
 	return ganojugador(tablero, 2, c, ultimajugada);	
 }
 
-
-
-// la función minimax devuelve un par, el primer elemento es la posición donde consigue el mejor valor posible
-// es decir la jugada óptima desde el tablero de entrada, el segundo elemento es el mejor valor que
-// se puede conseguir al hacer esa jugada.
-pair<int,int> minimax(int rows, int columns, int c, int p, vector<vector<int>> tablero,  bool maximizo, int ultimajugada){
-	//el 888888888888 y -88888888 son numeros de relleno, porque aun no inicialize. Uso el negativo para que no
-	//afecte al tomar maximo entre 0,1 y -1
-	
-	
-	if(gane(tablero, c, ultimajugada)) {return make_pair(88888888,1);} //gane
-	
-	if(perdi(tablero, c, ultimajugada)) {return make_pair(88888888,-1);} //perdi
-	
-	if(p==0) { return make_pair(88888888,0);} //empate, no hay fichas
-	
-	vector<std::pair<int,int> > posibles;
-	for(int h=0;h<columns;++h){
-		if(tablero[h].size()<rows){ posibles.push_back(make_pair(h,-88888888));}
-		//el -88888888 es que aun no se el puntaje que da ese nodo en el arbol minimax
-	}
-	
-	if(posibles.size()==0) {return make_pair(88888888,0);} //empate, se lleno el tablero
-
-	//hago la recursion, llamando al siguiente en el arbol de minimax
-	for(int i=0; i<posibles.size(); ++i){
-		vector<vector<int>> tablero2=tablero;
-		int quienva;
-		if(maximizo) {quienva=1;} else {quienva=2;}
-		tablero2[posibles[i].first].push_back(quienva); //juego el o yo, segun minimice o maximice respectivamente.
-		if (maximizo) {
-			posibles[i].second = minimax(rows, columns, c, p-1, tablero2, not maximizo, posibles[i].first).second;
-		} else {
-			posibles[i].second = minimax(rows, columns, c, p, tablero2, not maximizo, posibles[i].first).second;
-			//PUSE P EN VEZ DE P-1, CUANDO JUEGA EL, QUE ES CUANDO MINIMIZO NO USO FICHA
-			
-		}
-	}
-	
-	if (maximizo) {
-		//agarro la columna que da el maximo y devuelvo.
-		int maxpos=posibles[0].first;
-		int maximo=posibles[0].second;
-		for (int h=1;h<posibles.size();++h) {
-			if (maximo<posibles[h].second) {maximo=posibles[h].second; maxpos=posibles[h].first;};
-		}
+int main(){
+	int c, columnas;
+	cin>> c;
+	cin>> columnas;
+	vector<vector<int>> tablero (columnas);	
+	for(int q=0;q<columnas;++q){
+		int x;
+		cin>> x;
+		while (x!=0){
+			tablero[q].push_back(x);
+			cin>>x;			
+			}
 		
-		return make_pair(maxpos, posibles[maxpos].second) ;//donde tenia que jugar y cual es el resultado en ese orden	
-	} else {
-		
-		//agarro la columna que da el minimo y devuelvo.
-		int minpos=posibles[0].first;
-		int minimo=posibles[0].second;
-		for (int h=1;h<posibles.size();++h) {
-			if (minimo>posibles[h].second) {minimo=posibles[h].second; minpos=posibles[h].first;};
-		}
-		
-		return make_pair(minpos, posibles[minpos].second) ;//donde tenia que jugar y cual es el resultado en ese orden
-
 	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-int main() {
-
-    //std::default_random_engine generator;
-    std::string msg, color, oponent_color, go_first;
-    int columns, rows, c, p, move;
-
-    while (true) {
-        color = read_str();
-        oponent_color = read_str();
-
-        columns = read_int();
-        rows = read_int();
-        c = read_int();
-        p = read_int();
-		//std::vector<int> board(columns);
-		vector<vector<int>>tablero (columns);
-		//la primer cordenada del tablero es la columna, y la segunda es la fila
-        
-        //for(int i=0; i<columns; ++i) board[i] = 0;
-
-        go_first = read_str();
-        if (go_first == "vos") {
-            move = minimax(rows, columns, c, p, tablero, true, -1).first;
-            tablero[move].push_back(1); //juego yo
-            //board[move]++;
-            --p;
-            send(move);
-        }
-
-        while (true) {
-            msg = read_str();
-            if (msg == "ganaste" || msg == "perdiste" || msg == "empataron") {
-                break;
-            }
-			tablero[std::stoi(msg)].push_back(2);//juega el
-            //board[std::stoi(msg)]++;
-            move = minimax(rows, columns, c, p, tablero, true, std::stoi(msg)).first;
-            tablero[move].push_back(1); //juego yo
-            //board[move]++;
-            --p;
-            send(move);
-        }
-    }
-
-    return 0;
-}
+	if( gane(tablero,c,1)) {cout<<"ganaste!"<<endl;}
+	
+	return 0;
+	}
