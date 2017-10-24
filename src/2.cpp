9c461fe00acbfd23a0b6ce2305b3338030421363
nicocheh <nicocheh@hotmail.com>
2017-10-24 15:34:07 -0300

    avances chebar por hoy

--- src/2.cpp
+++ src/2.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 std::random_device rd;
 std::mt19937 generator(rd());
+#define INF std::numeric_limits<float>::max();
+#define INFneg std::numeric_limits<float>::min();
 
 void send(const std::string& msg) {
     std::cout << msg << std::endl;
@@ -44,22 +46,227 @@ std::string read_str() {
 
 
 
+bool ganojugador(vector<vector<int>> tablero, int i, int c, int ultimajugada){
+	
+	 if(ultimajugada==-1){if(c==1) {return true;}else{return false;}}
+     //CAMBIO CHEBAR, AGREGUE ESTO, COSA QUE SI HAY QUE PONER UNA SOLA Y SOS EL PRIMERO EN JUGAR YA GANASTE
+     //Por si es la primer jugada, nadie gano aun
+	 if (tablero[ultimajugada][tablero[ultimajugada].size()-1]!=i) return false; //tablero[j].size()-1 es la ultima fila con fichas de la columna j
+	 bool esMio = true;
+		
+		
+		 //checkear si hay c en linea en la columna
+		 if (tablero[ultimajugada].size() < c){
+			 esMio=false;//si la columna tiene menos fichas que c, no checkeamos
+		 }else{
+			 int k=tablero[ultimajugada].size()-1; //k es la fila que voy a chequear ahora
+			 while (tablero[ultimajugada].size()-k<=c && esMio){
+				 if (tablero[ultimajugada][k] != i) esMio=false;
+				 --k;
+			 }
+			 if (esMio) return true;
+		 }
+		 
+		
+		///////////////////////////////////////////////////////////////////////////////////////////// 
+		 //checkear si hay c en linea en la fila
+		 esMio = true;
+		 int k=ultimajugada;
+		 int contador=0;
+		 while (esMio && k>=0 && contador<c){
+			 //calculamos cuantos de los que estan a la izquierda de j son iguales
+			 if (tablero[k].size()>=tablero[ultimajugada].size() && tablero[k][tablero[ultimajugada].size()-1]==i){
+				 ++contador;
+			 }else{
+				 esMio = false;
+			 }
+			 --k;
+		 }
+		 ///////////////////////Calcular a derecha//////////////////////////////
+		 k=ultimajugada;
+		 --contador;
+		 esMio=true;
+		 while (esMio && k<tablero.size() && contador<c){
+			 //calculamos a la derecha de j
+			 if (tablero[k].size()>=tablero[ultimajugada].size() && tablero[k][tablero[ultimajugada].size()-1]==i){
+				 ++contador;
+			 }else{
+				 esMio = false;
+			 }
+			 ++k;
+		 }
+		 if (contador==c) return true;
+		 //Por la guarda del while, o bien contador es  <c (salimos del while por alguna de las otras razones)
+		 //o bien es igual a c por lo que no tiene sentido que el checkeo sea con >= 
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		 //checkear si hay c en linea en diagonal hacia arriba (de izq a der)
+		 esMio = true;
+		 int col=ultimajugada;
+		 int fil=tablero[ultimajugada].size()-1;
+		 contador=0;
+		 while (esMio && col>=0 && fil>=0 && contador<c){
+			 //calculamos cuantos de los que estan a la izquierda de j son iguales
+			 if (tablero[col].size()>=tablero[ultimajugada].size()-(ultimajugada-col) && tablero[col][fil]==i){
+			 	//yo pondría si empieza a funcionar mal tablero[col].size()>=fil+1
+			 	 ++contador;
+			 }else{
+				 esMio = false;
+			 }
+			 --col;
+			 --fil;
+		 }
+		 ///////////////////////Calcular a derecha//////////////////////////////
+		 col=ultimajugada;
+		 fil=tablero[ultimajugada].size()-1;
+		 --contador;
+		 esMio=true;
+		 
+		 while (esMio && col<tablero.size() && contador<c){
+			 //calculamos a la derecha de j
+			 if (tablero[col].size()>=tablero[ultimajugada].size()+(col-ultimajugada) && tablero[col][fil]==i){
+				++contador;
+			 }else{
+				 esMio = false;
+			 }
+			 ++col;
+			 ++fil;
+		 }
+		 if (contador==c) return true;
+		 
+		 //////////////////////////////////////////////////////////////////////////////////////////////
+		 //checkear si hay c en linea en diagonal hacia abajo
+		 esMio = true;
+		 col=ultimajugada;
+		 fil=tablero[ultimajugada].size()-1;
+		 contador=0;
+		 while (esMio && col>=0 && contador<c){
+			 //calculamos cuantos de los que estan a la izquierda de j son iguales
+			 if (tablero[col].size()>=tablero[ultimajugada].size()+(ultimajugada-col) && tablero[col][fil]==i){
+				 ++contador;
+			 }else{
+				 esMio = false;
+			 }
+			 --col;
+			 ++fil;
+		 }
+		 ///////////////////////Calcular a derecha//////////////////////////////
+		 col=ultimajugada;
+		 fil=tablero[ultimajugada].size()-1;
+		 --contador;
+		 esMio=true;
+		 
+		 while (esMio && col<tablero.size() && fil>=0 && contador<c){
+			 //calculamos a la derecha de j
+			 if (tablero[col].size()>=tablero[ultimajugada].size()-(col-ultimajugada) && tablero[col][fil]==i){
+				 ++contador;
+			 }else{
+				 esMio = false;
+			 }
+			 ++col;
+			 --fil;
+		 }
+		 if (contador==c) return true;
+		 
+	 return false;
+}
+
+
+bool gane ( const vector<vector<int>>& tablero, int c, int ultimajugada){
+	return ganojugador(tablero, 1, c, ultimajugada);	
+}
+	
+bool perdi (const vector<vector<int>>& tablero, int c, int ultimajugada){
+	return ganojugador(tablero, 2, c, ultimajugada);	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int enborde(int rows,int columns,const vector<vector<int>>& tablero, int i){
+	int enborde=0;
+	for (int h=0; h<columns; ++h){
+		if (tablero[h].size()!=0 && tablero[h][0]==i) {++enborde;} //en la fila de abajo
+		if (tablero[h].size()==rows && tablero[h][rows-1]==i) {++enborde;} //en la fila de arriba de todo
+	}
+	for (int h=0; h<tablero[0].size(); ++h){
+		if(tablero[0][h]==i){++enborde;}//en primera columna
+	}
+	for (int h=0; h<tablero[0].size(); ++h){
+		if(tablero[columns-1][h]==i){++enborde;}//en ultima columna
+	}
+	if (tablero[0].size()!=0 && tablero[0][0]==i) {--enborde;} //conte dos veces la esquina inf izq
+	if (tablero[columns-1].size()!=0 && tablero[columns-1][0]==i) {--enborde;} //conte dos veces la esquina inf der
+	if (tablero[0].size()==rows && tablero[0][rows-1]==i) {--enborde;} //conte dos veces la esquina sup izq
+	if (tablero[columns-1].size()==rows && tablero[columns-1][rows-1]==i) {--enborde;} //conte dos veces la esquina sup der
+	return enborde;
+	
+	}
+
+
+int esquinas(int rows,int columns,const vector<vector<int>>& tablero, int i){
+	int enesquina=0;
+	if (tablero[0].size()!=0 && tablero[0][0]==i) {--enesquina;} //la esquina inf izq
+	if (tablero[columns-1].size()!=0 && tablero[columns-1][0]==i) {--enesquina;} //la esquina inf der
+	if (tablero[0].size()==rows && tablero[0][rows-1]==i) {--enesquina;} //la esquina sup izq
+	if (tablero[columns-1].size()==rows && tablero[columns-1][rows-1]==i) {--enesquina;} //la esquina sup der
+	return enesquina;
+	
+	}
+
 
 
 //RECORDAR TODOS LOS PARAMETROS QUE AGREGAMOS, PONERSELOS EN CADA LLAMADA DE PUNTAJES ¿Y EN LA DE PARAMETRIZABLE?
 
 
-int puntaje(int rows, int columns, int c, int p, const vector<vector<int>>& tablero, int ultimajugada){
-	//ideas de puntajes:
+float puntaje(int rows, int columns, int c, int p, const vector<vector<int>>& tablero, int ultimajugada){
 	
-	//cantidad de fichas en los bordes
-	//cantidad de fichas en las esquinas
-	//cantidad de fichas en el centro
-	//cantidad de fichas consecutivas, donde agregar una suma mucho mas. (¿mirar solo las utiles? i.e las extensibles a un 4 en linea)
+	if( gane(tablero,c,ultimajugada)) {return INF;} //si gane da infinito CHECKEAR Q ESTE BIEN Y DE ESO POSTA
+	if( perdi(tablero,c,ultimajugada)) {return INFneg;} //si gane da -infinito CHECKEAR Q ESTE BIEN Y DE ESO POSTA
+	
+	//cuento los que estan en el borde, mias y de el.
+	int enborde1=enborde(rows,columns,tablero,1);
+	int enborde2=enborde(rows,columns,tablero,2);
+	//cuento la cantidad de fichas que estan en una esquina, mias y de el.
+	int esquinas1=esquinas(rows,columns,tablero,1);
+	int esquinas2=esquinas(rows,columns,tablero,2);
+	
+	
+	//ideas de puntajes:
+	//cantidad de fichas en los bordes HECHO
+	//cantidad de fichas en las esquinas HECHO
+	//cantidad de fichas en el centro (definir mejor... en la del medio solo? hasta que altura las cuento?)
+	
+	//cantidad de fichas consecutivas, donde agregar una suma mucho mas. (¿mirar solo las utiles? i.e las extensibles a un 4 en linea. Sirve contar unas ya tapadas?)
 	//cantidad de posibles 4 en linea que puedo formar
-	//gano +inf y si pierdo es -inf
+	
+	//gano +inf y si pierdo es -inf HECHO
 
-	//tratar de maximizar mi putntaje, o minimizar el de el, o maximizar la diferencia (me parece mejor la ultima pero puede ser otro parámetro)
+	//tratar de maximizar mi putntaje, o minimizar el de el, o maximizar la diferencia (me parece mejor la ultima pero puede ser otro parámetro y que decida entre las tres)
 	
 	
 	
@@ -98,7 +305,7 @@ int parametrizable (int rows, int columns, int c, int p, vector<vector<int>>& ta
 	
 	//si o si hay una jugada posibles pues sino seria empate
 	tablero[posibles[0]].push_back(1);
-	int maximopuntaje=puntaje(rows, columns, c, p, tablero, ultimajugada);
+	float maximopuntaje=puntaje(rows, columns, c, p, tablero, ultimajugada);
 	int maxpos=posibles[0];
 	tablero[posibles[0]].pop_back();
 	
@@ -174,3 +381,4 @@ int main() {
 
     return 0;
 }
+
